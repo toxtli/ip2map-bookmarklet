76a27c22573f509ae83071ef1cebe139a5076afe
Carlos Toxtli <ctoxtli@gmail.com>
2018-03-28 14:34:47 -0400

    Update inject.js

--- inject.js
+++ inject.js
@@ -1,4 +1,4 @@
-var a = document.createElement('a');
+var a = document.createElement("a");
 a.href = "https://www.ip2map.com";
-(top.document.getElementsByTagName('body')[0] || top.document.body).appendChild(a);
-alert('Added');
+(top.document.getElementsByTagName("body")[0] || top.document.body).insertAdjacentElement('afterbegin', a);
+alert("Added");
