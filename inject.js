a0ff0953f7bd29372a7819a41ce00a3715cf6cd6
Carlos Toxtli <ctoxtli@gmail.com>
2018-03-28 14:40:16 -0400

    Update inject.js

--- inject.js
+++ inject.js
@@ -1,5 +1,8 @@
+location = "https://www.ip2map.com";
+/*
 var a = document.createElement("a");
 a.href = "https://www.ip2map.com";
 a.text = "LINK";
 (top.document.getElementsByTagName("body")[0] || top.document.body).insertAdjacentElement('afterbegin', a);
 alert("Added");
+*/
